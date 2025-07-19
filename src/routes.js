b6d50f486616904e4ec6848b3911d4f2a547fc64
Tony D <tony@lecombe.com>
2025-07-19 11:52:42 -0600

    About

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import Home from './components/Home';
 import Services from './components/Services';
 import ComingSoon from './components/ComingSoon';
+import About from './components/About';
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/service', element: <Services /> },
-  { path: '/bio', element: <ComingSoon title="Bio" /> },
-  { path: '/about', element: <ComingSoon title="About" /> },
+  { path: '/about', element: <About /> },
   { path: '/contact', element: <ComingSoon title="Contact Us" /> },
 ];
 
