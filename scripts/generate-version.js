0a769f3f5544b55b967e4a8dd856eec3621b2c76
Tony D <tony@lecombe.com>
2025-07-22 11:37:26 -0600

    Added versioning

---
+++ scripts/generate-version.js
@@ -0,0 +1,4 @@
+// scripts/generate-version.js
+const { version } = require('../package.json');
+const fs = require('fs');
+fs.writeFileSync('src/version.js', `export default "${version}";\n`);
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+// filepath: /Users/tonyduncombe/Development/psha/src/components/Footer.js
+import React from 'react';
+
+const Footer = ({ children }) => (
+  <footer style={{ marginTop: 'auto', padding: '1rem 0', textAlign: 'center' }}>
+    {children}
+  </footer>
+);
+
+export default Footer;
